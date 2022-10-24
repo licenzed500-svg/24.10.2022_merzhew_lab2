989d0e228e973e722a175642a3c1ccb09fbe136a
Maksim Merzhew <79693216+licenzed500-svg@users.noreply.github.com>
2022-10-24 16:02:03 +0300

    Update dice_6.cpp

--- dice_6.cpp
+++ dice_6.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+
+
+short randto()
+{
+    short min = 1;
+    short max = 6;
+    return min + rand() % (max-min);
+}
+
+void data_out(short dice)
+{
+    std::cout << "it's dice " << dice << "\n"
+}
+
+int main()
+{
+    srand(time(NULL));
+    short dice = randto();
+    data_out(dice);
+    return 0;
+}
 
